8b10df9975a79ce238c2c47cf8d183434c92c4b9
Sgeo <sgeoster@gmail.com>
2017-08-10 23:34:35 -0400

    append -> write, and syncing. This is the working version.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use byteorder::{ByteOrder, BigEndian, ReadBytesExt, WriteBytesExt};
 const START: u64 = 0x08EDB7; // First moov, based on manual inspection.
 
 fn main() {
-    let mut SCIGUY = fs::OpenOptions::new().read(true).append(true).open("SCIGUY.MOV").expect("Unable to open SCIGUY.MOV!");
+    let mut SCIGUY = fs::OpenOptions::new().read(true).write(true).open("SCIGUY.MOV").expect("Unable to open SCIGUY.MOV!");
     SCIGUY.seek(SeekFrom::Start(0)).expect("Unable to seek to start!");
     SCIGUY.write_all(b"\x00\x00\x00\x01skip").expect("Unable to write skip atom header!");
     let mut current: u64 = START;
@@ -36,6 +36,7 @@ fn length_to_header(length: u64) -> [u8; 8] {
 fn convert(SCIGUY: &mut File, offset: u64) {
     SCIGUY.seek(SeekFrom::Start(8)).expect("Unable to seek to write new header!");
     SCIGUY.write_all(&length_to_header(offset)).expect("Unable to write size to new header!");
+    SCIGUY.sync_all().expect("Unable to sync!");
     Command::new("ffmpeg")
         .arg("-i")
         .arg("SCIGUY.MOV")
