29b2c68efdf50b815d04a21a589535832640312f
eientei95 <einstein95@users.noreply.github.com>
2025-05-13 10:40:53 +1200

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate byteorder;
 
-use std::io::prelude::*;
+use byteorder::{BigEndian, ByteOrder, ReadBytesExt};
 use std::fs;
 use std::fs::File;
 use std::io::SeekFrom;
+use std::io::prelude::*;
 use std::process::Command;
-use byteorder::{ByteOrder, BigEndian, ReadBytesExt};
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -14,19 +14,33 @@ fn main() {
         std::process::exit(1);
     }
     let filename = &args[1];
-    let mut mmfile = fs::OpenOptions::new().read(true).write(true).open(filename).expect("Unable to open the specified file!");
-    mmfile.seek(SeekFrom::Start(0)).expect("Unable to seek to start!");
-    mmfile.write_all(b"\x00\x00\x00\x01skip").expect("Unable to write skip atom header!");
-    mmfile.seek(SeekFrom::Start(0x10)).expect("Unable to seek past header!");
-    
+    let mut mmfile = fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(filename)
+        .expect("Unable to open the specified file!");
+    mmfile
+        .seek(SeekFrom::Start(0))
+        .expect("Unable to seek to start!");
+    mmfile
+        .write_all(b"\x00\x00\x00\x01skip")
+        .expect("Unable to write skip atom header!");
+    mmfile
+        .seek(SeekFrom::Start(0x10))
+        .expect("Unable to seek past header!");
+
     let (header, num_offsets) = if filename.to_lowercase().ends_with(".mmq") {
         let mut header = vec![0; 20];
-        mmfile.read_exact(&mut header).expect("Unable to read header!");
+        mmfile
+            .read_exact(&mut header)
+            .expect("Unable to read header!");
         let num_offsets = BigEndian::read_u16(&header[18..20]);
         (header, num_offsets)
     } else {
         let mut header = vec![0; 12];
-        mmfile.read_exact(&mut header).expect("Unable to read header!");
+        mmfile
+            .read_exact(&mut header)
+            .expect("Unable to read header!");
         let num_offsets = BigEndian::read_u16(&header[10..12]);
         (header, num_offsets)
     };
@@ -37,21 +51,32 @@ fn main() {
     }
 
     let version = BigEndian::read_u16(&header[2..4]);
-    println!("Version: {}, Number of file offsets: {}", version, num_offsets);
+    println!(
+        "Version: {}, Number of file offsets: {}",
+        version, num_offsets
+    );
 
     let mut offsets = Vec::new();
     for _ in 0..num_offsets {
-        let offset = mmfile.read_u32::<BigEndian>().expect("Unable to read file offset!");
+        let offset = mmfile
+            .read_u32::<BigEndian>()
+            .expect("Unable to read file offset!");
         offsets.push(offset);
     }
-    let _filelength = mmfile.read_u32::<BigEndian>().expect("Unable to read file length!");
+    let _filelength = mmfile
+        .read_u32::<BigEndian>()
+        .expect("Unable to read file length!");
     if filename.to_lowercase().ends_with(".mmq") {
-        mmfile.seek(SeekFrom::Current(0x4)).expect("Unable to seek past junk offset!");
+        mmfile
+            .seek(SeekFrom::Current(0x4))
+            .expect("Unable to seek past junk offset!");
     }
     let mut filenames = Vec::new();
     for _ in 0..num_offsets {
         let mut filename = [0; 0x20];
-        mmfile.read_exact(&mut filename).expect("Unable to read filename!");
+        mmfile
+            .read_exact(&mut filename)
+            .expect("Unable to read filename!");
         let filename_str = String::from_utf8_lossy(&filename);
         let filename_str = match filename_str.find('\0') {
             Some(pos) => &filename_str[..pos],
@@ -91,8 +116,12 @@ fn length_to_header(length: u64) -> [u8; 8] {
 
 fn convert(mmfile: &mut File, offset: u64, filename: &str, output_filename: &str) {
     println!("Converting {:?} at @{}", output_filename, offset);
-    mmfile.seek(SeekFrom::Start(8)).expect("Unable to seek to write new header!");
-    mmfile.write_all(&length_to_header(offset)).expect("Unable to write size to new header!");
+    mmfile
+        .seek(SeekFrom::Start(8))
+        .expect("Unable to seek to write new header!");
+    mmfile
+        .write_all(&length_to_header(offset))
+        .expect("Unable to write size to new header!");
     mmfile.sync_all().expect("Unable to sync!");
     Command::new("ffmpeg")
         .arg("-i")
@@ -100,5 +129,6 @@ fn convert(mmfile: &mut File, offset: u64, filename: &str, output_filename: &str
         .arg("-c")
         .arg("copy")
         .arg(format!("{}.mov", output_filename))
-        .output().expect("Unable to call ffmpeg!");
+        .output()
+        .expect("Unable to call ffmpeg!");
 }
